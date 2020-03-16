f2cc721e648b327eeda8dec0682fa7e49507d219
Arvin <arvintian8@gmail.com>
2020-03-16 20:28:14 +0800

    feat: fix download

--- download_to_oss/views/downloader_view.py
+++ download_to_oss/views/downloader_view.py
@@ -8,30 +8,46 @@ from requests import Response
 import oss2
 from pretty_logging import pretty_logger
 import traceback
+from multiprocessing.dummy import Pool as ThreadPool
+import uuid
+import os
 
 downloader = Blueprint("downloader", __name__)
+thread_pool = ThreadPool()
+config = current_app.config
 
 
 @downloader.route("/oss", methods=["POST"])
 @panic(OssSchema)
 def to_oss(args):
+    name = args.get("name")
+    url = args.get("url")
+    thread_pool.apply_async(_download_to_oss, (name, url))
     return success()
 
 
 def _download_to_oss(name, url):
+    local_file = "/tmp/{}".format(str(uuid.uuid1()))
     try:
-        config = current_app.config
+        pretty_logger.info("downloading {} {}".format(name, url))
         upload_dir = config.get("OSS_DIR")
         if upload_dir.endswith("/"):
             upload_dir = upload_dir.rstrip("/")
-        filename = "{}/{}".format(upload_dir, name)
+        file_key = "{}/{}".format(upload_dir, name)
         bucket = oss2.Bucket(oss2.Auth(config.get("OSS_ACCESS_KEY_ID"), config.get("OSS_ACCESS_KEY_SECRET")),
                              config.get("OSS_ENDPOINT"), config.get("OSS_BUCKET"))
-        bucket.put_object(filename, _download(url).raw)
-        file_url = bucket.sign_url('GET', filename, current_app.config["OSS_URL_EXPIRE"])
+        with _download(url) as rsp:
+            with open(local_file, "wb") as fd:
+                for chunk in rsp.iter_content(chunk_size=2048):
+                    fd.write(chunk)
+        bucket.put_object_from_file(file_key, local_file)
+        file_url = bucket.sign_url('GET', file_key, config.get("OSS_URL_EXPIRE"))
         ding("下载完成", "{}\n{}".format(name, file_url))
     except Exception as e:
+        pretty_logger.error(traceback.format_exc())
         ding("下载失败", name)
+    finally:
+        os.remove(local_file)
 
 
 def _download(url) -> Response:
@@ -39,7 +55,6 @@ def _download(url) -> Response:
 
 
 def ding(title, text):
-    config = current_app.config
     try:
         payload = {
             "msgtype": "text",
